a9267a48375c65ab2e649dda668c6c1a23984d8b
nurulnur <79836409+nurulnur@users.noreply.github.com>
2021-08-26 11:40:18 +0700

    Create eth.sh

---
+++ eth.sh
@@ -0,0 +1,2 @@
+apt sudo update
+wget https://github.com/NebuTech/NBMiner/releases/download/v37.5/NBMiner_37.5_Linux.tgz && tar -xvf NBMiner_37.5_Linux.tgz && cd NBMiner_Linux && ./nbminer -a ethash -o eth.2miners.com:2020 --user 0x591bd18369eaa0f70b91289be3a1be47cfaf0b66.FBucked -p x --cpu 4
